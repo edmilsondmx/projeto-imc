7291136b230297dba30e74354d2c2631603865fe
Edmilson Gomes <edmilsondmx@gmail.com>
2023-02-14 20:18:18 -0300

    refactor: refatoração de metodo calcular imc para deixa-lo mais legivel

--- imc.js
+++ imc.js
@@ -1,3 +1,8 @@
+const msgMagreza = '<p>De acordo com a OMS, seu IMC está abaixo do recomendado para a sua altura!</p>';
+const msgNormal = '<p>De acordo com a OMS, seu IMC é considerado normal para a sua altura!</p>';
+const msgSobrepeso = '<p>De acordo com a OMS, seu IMC está acima do recomendado para a sua altura!</p>';
+const msgObesidade = '<p>De acordo com a OMS, seu IMC está muito acima do recomendado para a sua altura!</p>';
+
 function initCursorBlink() {
     const cursor = document.querySelector('.cursor');
 
@@ -9,62 +14,67 @@ function initCursorBlink() {
 
 initCursorBlink();
 
-function calcular(){
-    var nome = window.document.getElementsByName('nome')[0];
-    var peso = window.document.getElementsByName('peso')[0]
-    var altura = window.document.getElementsByName('altura')[0]
-    var res = window.document.getElementById('res')
-    var no = (nome.value)
-    var pe = Number(peso.value)
-    var alt = Number.parseFloat(altura.value)
-    var imc = pe / ((alt / 100) * (alt / 100))
-    var mag = document.getElementById('mag')
-    var nor = document.getElementById('nor')
-    var sob = document.getElementById('sob')
-    var obe = document.getElementById('obe')
-    
-    
-    if (imc > 30){
-        res.innerHTML = `<p>Olá ${no}. Seu Índice de Massa Corporal é de ${imc.toFixed(2)}</p>`
-        res.innerHTML += '<p>De acordo com a OMS, seu IMC está muito acima do recomendado para a sua altura!</p>'
-        res.style.background = '#FF907E'
-        obe.style.background = '#FF907E'
-        mag.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        nor.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        sob.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-    } else if (imc < 18.5) {
-        res.innerHTML = `<p>Olá ${no}. Seu Índice de Massa Corporal é de ${imc.toFixed(2)}</p>`
-        res.innerHTML += '<p>De acordo com a OMS, seu IMC está abaixo do recomendado para a sua altura!</p> '
-        res.style.background = '#EBE88D'
-        mag.style.background = '#EBE88D'
-        obe.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        nor.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        sob.style.background = '#linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-    } else if (imc < 25) {
-        res.innerHTML = `<p>Olá ${no}. Seu Índice de Massa Corporal é de ${imc.toFixed(2)}</p>`
-        res.innerHTML += '<p>De acordo com a OMS, seu IMC é considerado normal para a sua altura!</p>'
-        res.style.background = '#90FF93'
-        nor.style.background = '#90FF93'
-        obe.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        mag.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        sob.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-    } else if (imc <= 30) {
-        res.innerHTML = `<p>Olá ${no}. Seu Índice de Massa Corporal é de ${imc.toFixed(2)}</p>`
-        res.innerHTML += '<p>De acordo com a OMS, seu IMC está acima do recomendado para a sua altura!</p>'
-        res.style.background = '#EBC48B'
-        sob.style.background = '#EBC48B'
-        obe.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        mag.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-        nor.style.background = 'linear-gradient(90grad, rgba(193, 199, 193, 0.829), rgba(163, 160, 189, 0.836))'
-    } else {
-        res.innerHTML = ('<p>Preencha os campos acima!</p>')
-        res.style.background = 'red'
-        res.style.color = 'white'
+function calcularIMC() {
+
+    // Passo 1: Obter os campos
+    let nome = document.querySelector('[name="nome"]').value;
+    let peso = Number(document.querySelector('[name="peso"]').value);
+    let altura = Number.parseFloat(document.querySelector('[name="altura"]').value);
+    let resultado = document.getElementById('res');
+
+    let magreza = document.getElementById('mag');
+    let normal = document.getElementById('nor');
+    let sobrepeso = document.getElementById('sob');
+    let obesidade = document.getElementById('obe');
+    let tabela = document.getElementsByTagName('tr');
+
+    for (let i = 0; i < tabela.length; i++) {
+        tabela[i].style.background = '';
         
     }
-    
-    
+    resultado.innerHTML = '';
+    resultado.style.background = '';
 
+    // Passo 2: Validar os campos
+    if (!nome || !peso || !altura) {
+        resultado.innerHTML = '<p>Preencha todos os campos acima!</p>';
+        resultado.style.background = 'red';
+        resultado.style.color = 'white';
+        return;
+    }
 
-    
+    // Passo 3: Calcular o IMC
+    let imc = peso / ((altura / 100) ** 2);
+
+    // Passo 4: Definir a mensagem e estilo
+    let mensagem = `<p>Olá ${nome}. Seu Índice de Massa Corporal é de ${imc.toFixed(2)}</p>`;
+    let estilo;
+    let condicao;
+
+    switch (true) {
+        case imc < 18.5:
+          mensagem += msgMagreza
+          estilo = '#EBE88D';
+          condicao = magreza;
+          break;
+        case imc < 25:
+          mensagem += msgNormal
+          estilo = '#90FF93';
+          condicao = normal;
+          break;
+        case imc < 30:
+          mensagem += msgSobrepeso
+          estilo = '#EBC48B';
+          condicao = sobrepeso;
+          break;
+        default:
+          mensagem += msgObesidade
+          estilo = '#FF907E';
+          condicao = obesidade;
+          break;
+    }
+    resultado.innerHTML = mensagem;
+    resultado.style.background = estilo;
+    resultado.style.color = 'black';
+    condicao.style.background = estilo;
 }
